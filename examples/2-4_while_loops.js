833782782d546f3e0497e67caffec64135ab6bb2
Jacob DeFalco <jake.defalco97@gmail.com>
2023-05-08 13:41:32 -0400

    loop example

---
+++ examples/2-4_while_loops.js
@@ -0,0 +1,10 @@
+"use strict";
+let num = 1;
+let i = 1;
+
+//the code between brackets will continue to run over and over until the conditions are met (i > 5)
+while (i <= 15) {
+  num = num * 2;
+  console.log(num);
+  i++;
+}
